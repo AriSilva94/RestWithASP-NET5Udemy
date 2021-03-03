0e39ec70ee6bce3cc4b9f2221da0c6c354c74d59
Ariovaldo <ariovaldo.bsjunior@gmail.com>
2021-03-03 15:29:25 -0300

    Finish db settings

--- RestWithASP-NET5Udemy/Services/Implementations/PersonServiceImplementation.cs
+++ RestWithASP-NET5Udemy/Services/Implementations/PersonServiceImplementation.cs
@@ -1,55 +1,84 @@
 ﻿using RestWithASP_NET5Udemy.Model;
+using RestWithASP_NET5Udemy.Model.Context;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace RestWithASP_NET5Udemy.Services.Implementations
 {
     public class PersonServiceImplementation : IPersonServices
     {
-        public Person Create(Person person)
+        private readonly MySQLContext _context;
+        public PersonServiceImplementation(MySQLContext context)
         {
-            return person;
-        }
-
-        public void Delete(long id)
-        {
-            
+            _context = context;
         }
 
         public List<Person> FindAll()
         {
-            List<Person> people = new List<Person>();
-            for (int i = 0; i < 8; i++)
-            {
-                Person person = new Person
-                {
-                    Id = i,
-                    FirstName = $"Ariovaldo - {i}",
-                    LastName = $"Silva - {i}",
-                    Address = $"Araraquara - São Paulo - {i}",
-                    Gender = "Male"
-                };
-
-                people.Add(person);
-            }
-
-            return people;
+            return _context.Persons.ToList();
         }
 
         public Person FindById(long id)
         {
-            return new Person
-            {
-                Id = 1,
-                FirstName = "Ariovaldo",
-                LastName = "Silva",
-                Address = "Araraquara - São Paulo",
-                Gender = "Male"
-            };
+            return _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
         }
+        public Person Create(Person person)
+        {
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
 
+            return person;
+        }
         public Person Update(Person person)
         {
+            if (!Exist(person.Id)) return new Person();
+
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
+
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+
             return person;
+        }
+        public void Delete(long id)
+        {
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+
+            if (result != null)
+            {
+                try
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        private bool Exist(long id)
+        {
+            return _context.Persons.Any(p => p.Id.Equals(id));
         }
     }
 }
